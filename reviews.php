e6a3881e289493da23199611100b7089ab833c1b
gossoudarev <igossoudarev@herzen.spb.ru>
2018-01-06 05:37:07 +0300

    prep3

--- reviews.php
+++ reviews.php
@@ -6,8 +6,9 @@
   $f = '<h2>Форма для заполнения... </h2>';
   if ($_SERVER['REQUEST_METHOD']==='POST') {
       $r = array_map(function($x){return htmlentities($x);}, [ $_POST['name']  ??  'Unknown user', $_POST['email']  ?? 'anon@mail.ru', $_POST['text']  ?? 'Пустое сообщение']);
-      $sql =  "INSERT  INTO `reviews` (`name`, `email`, `text`) VALUES ('{$r[0]}','{$r[1]}', '{$r[2]}');";
-      $result = $conn -> query($sql);
+      $sql =  "INSERT  INTO `reviews` (`name`, `email`, `text`) VALUES (?, ?, ?);";
+      $result = $conn -> prepare($sql);
+      $result -> execute($r);
       if ($result) {
          $f = '<style>.right {width: 60%; margin-left: 35%; zoom: 80%}</style><div class="right"><h2>Добавить ещё один отзыв...</h2></div>';
          $i = '<h3>Данные успешно вставлены!</h3>';
